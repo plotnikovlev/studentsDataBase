c306c4b59154d51b47c62591f8fde14ad26b1e2d
plotnikovlev <153212296+plotnikovlev@users.noreply.github.com>
2025-09-24 11:06:20 +0300

    Added test for FindLowestGpa fucntion.

--- test.cpp
+++ test.cpp
@@ -24,6 +24,25 @@ TEST(StudentDBTest, FindHighestGpa_BasicTest) {
 }
 
 
+// Тест для поиска студента с наинизшим баллом
+TEST(StudentDBTest, FindLowestGpa_BasicTest) {
+    std::vector<Student> db = {
+        {"Alice", 20, "CS", 3.8},
+        {"Bob", 22, "EE", 3.9},
+        {"Charlie", 21, "ME", 3.5}
+    };
+    const Student* worstStudent = &db[0];
+    for (const auto& s : db) {
+        if (s.gpa < worstStudent->gpa) {
+            worstStudent = &s;
+        }
+    }
+    ASSERT_EQ(worstStudent->name, "Charlie");
+    ASSERT_DOUBLE_EQ(worstStudent->gpa, 3.5);
+}
+
+
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
